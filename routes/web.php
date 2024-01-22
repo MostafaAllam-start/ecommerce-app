838626b3ccbdc62258f77eca73aa657227e64594
ARCHIVES <ARCHIVES@NSPO.MIL>
2024-01-22 17:10:09 +0200

    adding admin views.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::prefix('/admin')->namespace('App\Http\Controller\Admin')->group(function(){
+    Route::get('dashboard', [AdminController::class, 'dashboard']);
+    Route::get('login', [AdminController::class, 'login']);
 });
 
 require __DIR__.'/auth.php';
